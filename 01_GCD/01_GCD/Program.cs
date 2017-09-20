e517290416da5af87b64c7e5adc633ee753435c9
psGringo <panteleevstas@gmail.com>
2017-09-20 19:36:34 +0500

    1-st commit

---
+++ 01_GCD/01_GCD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("gcd(10,5)="+gcd(10,5));
+            Console.Read();
+        }
+
+        public static int gcd(int p, int q)
+        {
+            if (q == 0) return p;
+            int r = p % q;
+            return gcd(q, r);
+        }
+
+    }
+}
