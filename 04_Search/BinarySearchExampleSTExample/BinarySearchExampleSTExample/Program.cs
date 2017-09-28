1ea18b7e5c33f8d40bbc47e0e2d6c0b8fa0f82cd
psGringo <panteleevstas@gmail.com>
2017-09-28 18:33:45 +0500

    bin search improvements

--- 04_Search/BinarySearchExampleSTExample/BinarySearchExampleSTExample/Program.cs
+++ 04_Search/BinarySearchExampleSTExample/BinarySearchExampleSTExample/Program.cs
@@ -10,11 +10,34 @@ namespace BinarySearchExampleSTExample
     {
         static void Main(string[] args)
         {
+            string[] a =  {
+                "S",
+                "E",
+                "A",
+                "R",
+                "C",
+                "H",
+                "E",
+                "X",
+                "A",
+                "M",
+                "P",
+                "L",
+                "E"
+            };
 
+            BinarySearchST<string, int> bst = new BinarySearchST<string, int>(13);
+            //adding
+            for (int i = 0; i < a.Length; i++)
+            {
+                bst.put(a[i], i);
+            }
+            bst.ConsoleDisplay();
+            Console.Read();
         }
     }
 
-    class BinarySearchST<Key, Value> where Key:IComparable
+    class BinarySearchST<Key, Value> where Key : IComparable
     {
         private Key[] keys;
         private Value[] vals;
@@ -28,21 +51,58 @@ namespace BinarySearchExampleSTExample
         {
             return N;
         }
-        /*
+
+        public bool isEmpty()
+        {
+            return (N == 0);
+        }
+
+
         public Value get(Key key)
         {
-
+            if (isEmpty()) return default(Value);
+            int i = rank<Key>(key, 0, keys.Length - 1);
+            if (i <= N && keys[i].CompareTo(key) == 0) return vals[i];
+            else return default(Value);
         }
-        */
 
-        public int rank(Key key, int lo, int hi)
+        public void put(Key key, Value val)
+        {
+            int i = rank<Key>(key, 0, keys.Length);
+            if (i <= N && keys[i].CompareTo(key) == 0) // if found and equal to just change value
+            {
+                vals[i]=val;
+                return;
+            }
+            for (int j = N; j > i; j--) // if not found - shift values
+            {
+                keys[j] = keys[j - 1];
+                vals[j] = vals[j - 1];
+            }
+            keys[i] = key;
+            vals[i] = val;
+            N++;
+        }
+
+        public int rank<T>(T key, int lo, int hi) where T : IComparable
         {
             if (hi < lo) return lo;
-            int mid = lo + (hi - lo) / 2;
-            int cmp = key.CompareTo(key);
-            if (cmp < 0) return rank(key,lo,mid-1);
-            if (cmp > 0) return rank(key, mid + 1, hi);
+            int mid = (hi - lo) / 2; //lo + (hi - lo) / 2;
+            int cmp = key.CompareTo(keys[mid]);
+            if (cmp < 0) { rank<T>(key, lo, mid - 1); }
+            else if (cmp > 0) { rank<T>(key, mid + 1, hi); }
             else return mid;
+            return -1;
+        }
+
+        public void ConsoleDisplay()
+        {
+            Console.WriteLine();
+            Console.WriteLine("key" + " " + "val");
+            for (int i=0;i<vals.Length;i++)
+            {
+                Console.WriteLine(keys[i]+" "+vals[i]);
+            }
         }
     }
 }
--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -10,14 +10,17 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-                      
-            int[] a = { 1, 3, 5, 6, 12 };
-            Array.Sort(a);                        
+            Random r = new Random();
+            //int[] a = new int[100];
+            //for (int i = 0; i < 100; i++) a[i] = r.Next(100);
+            int[] a = { 1, 3, 5, 6, 12,30,40,55,16,18,20 };
+            //int[] a = { 1, 5, 3 };
+            Array.Sort(a);
             if (BinarySearch(5, a) != -1) Console.WriteLine("Non recursive BS: Element found");
             if (BinarySearchGeneric<int>(5, a) != -1) Console.WriteLine("Non recursive Generic BS: Element found");
-            if (BinarySearchRecursive(5, a, 0,a.Length) != -1) Console.WriteLine("Recursive BS: Element found");
+            if (BinarySearchRecursive(5, a, 0, a.Length) != -1) Console.WriteLine("Recursive BS: Element found");
             if (BinarySearchRecursiveGeneric<int>(5, a, 0, a.Length) != -1) Console.WriteLine("Recursive Generic BS: Element found");
-            
+
             Console.ReadLine();
 
         }
@@ -25,9 +28,9 @@ namespace BinarySearch
         static int BinarySearch(int key, int[] a)
         {
             int low = 0;
-            int hi = a.Length - 1;           
-            while (low<=hi)
-            {            
+            int hi = a.Length - 1;
+            while (low <= hi)
+            {
                 int mid = (low + hi) / 2;
                 if (key < a[mid]) hi = mid - 1;
                 else if (key > a[mid]) low = mid + 1;
@@ -36,17 +39,16 @@ namespace BinarySearch
             return -1;
         }
 
-        public static  int BinarySearchRecursive(int key,int[] a, int lo, int hi)
+        public static int BinarySearchRecursive(int key, int[] a, int lo, int hi)
         {
             if (hi < lo) return lo;
-            int mid = lo + (hi - lo) / 2;            
-            if (key < a[mid]) BinarySearchRecursive(key, a, lo, mid - 1);
-            else if (key > a[mid]) BinarySearchRecursive(key, a, mid + 1, hi);
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) return BinarySearchRecursive(key, a, lo, mid - 1);            
+            else if (key > a[mid]) return BinarySearchRecursive(key, a, mid + 1, hi);
             else return mid;
-            return -1;
         }
 
-        static int BinarySearchGeneric<T>(T key, int[] a) where T:IComparable
+        static int BinarySearchGeneric<T>(T key, int[] a) where T : IComparable
         {
             int low = 0;
             int hi = a.Length - 1;
@@ -55,21 +57,20 @@ namespace BinarySearch
                 int mid = (low + hi) / 2;
                 int cmp = key.CompareTo(a[mid]);
                 if (cmp < 0) { hi = mid - 1; }
-                else if (cmp > 0) { low = mid + 1; }                
+                else if (cmp > 0) { low = mid + 1; }
                 else return mid;
             }
             return -1;
         }
 
-        public static int BinarySearchRecursiveGeneric<T>(T key, int[] a, int lo, int hi) where T:IComparable
+        public static int BinarySearchRecursiveGeneric<T>(T key, int[] a, int lo, int hi) where T : IComparable
         {
-            if (hi < lo) return lo;
+            //  if (hi < lo) return lo;
             int mid = lo + (hi - lo) / 2;
             int cmp = key.CompareTo(a[mid]);
-            if (cmp < 0) { BinarySearchRecursiveGeneric(key, a, lo, mid - 1); }
-            else if (cmp > 0) { BinarySearchRecursiveGeneric(key, a, mid + 1, hi); }
+            if (cmp < 0) { return BinarySearchRecursiveGeneric(key, a, lo, mid - 1); }
+            else if (cmp > 0) { return BinarySearchRecursiveGeneric(key, a, mid + 1, hi); }
             else return mid;            
-            return -1;
         }
     }
 }
