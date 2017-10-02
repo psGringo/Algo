4cc2d4e83afc66fcdaed71bc3811a49131f593e5
psGringo <panteleevstas@gmail.com>
2017-10-02 19:51:35 +0500

    Graph class

---
+++ 05_Graph/Graph/Graph/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph("123.txt");
+            Console.WriteLine(g.toString());
+            Console.Read();
+        }
+    }
+}
