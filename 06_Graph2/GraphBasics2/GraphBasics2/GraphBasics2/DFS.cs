fd80cbf485c3bd59dce8c09fbde928e4bae781cb
psGringo <panteleevstas@gmail.com>
2017-12-30 04:37:52 +0500

    update

---
+++ 06_Graph2/GraphBasics2/GraphBasics2/GraphBasics2/DFS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphBasics2
+{
+    class DFS
+    {
+        public Boolean[] IsMarked { get; set; }
+        public int Count { get; set; }
+        public DFS(Graph G, int v)
+        {
+            this.IsMarked[] = new Boolean[G.Vertices()];
+        }
+        private void IsSuchVerticle(Graph G, int v) // is such v in this graph?
+        {
+            IsMarked[v] = true;
+            Count++;
+            foreach (var w in G.adj[v]) IsSuchVerticle(G, w);
+        }
+    }
+
+}
